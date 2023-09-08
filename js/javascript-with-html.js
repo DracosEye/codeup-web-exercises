213fee5dbbf543da147dd8e2feb862177f3c0312
Ben Heller <benheller1016@gmail.com>
2023-09-08 10:49:14 -0500

    Complete JS with HTML lecture

--- js/javascript-with-html.js
+++ js/javascript-with-html.js
@@ -6,7 +6,7 @@ let myNumber = 7;
 
 // alert("Hello, I am an alert!");
 
-/*==== PROMPT ===== */
+/*==== CONFIRM ===== */
 
 // let userChoice = confirm("Wait! Don't leave yet! Can I chat with you?");
 //
@@ -15,3 +15,18 @@ let myNumber = 7;
 // } else {
 //     console.log("boooo");
 // }
+
+/*==== PROMPT ===== */
+
+let currentSpeed = prompt("How fast are you driving?");
+let plannedSpeed = prompt("How fast do you want to drive?");
+let distanceLeft = prompt("How many miles do you have left?");
+
+let timeAtCurrentSpeed = (distanceLeft / currentSpeed) * 60;
+let timeAtPlannedSpeed = (distanceLeft / plannedSpeed) * 60;
+
+let timeSaved = timeAtCurrentSpeed - timeAtPlannedSpeed;
+
+//Output with template literals
+console.log(`At your current speed, you will take ${timeAtCurrentSpeed.toFixed(0)} minutes.`);
+console.log(`At your planned speed, you will take ${timeAtPlannedSpeed.toFixed(0)} minutes. You will save ${timeSaved} minutes.`);
