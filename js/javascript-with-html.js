18a0838f86e92f276df3abee9868d423e597f1eb
Ben Heller <benheller1016@gmail.com>
2023-09-08 10:22:42 -0500

    cover alert and prompt

---
+++ js/javascript-with-html.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let myNumber = 7;
+
+/*==== ALERT ====== */
+
+// alert("Hello, I am an alert!");
+
+/*==== PROMPT ===== */
+
+// let userChoice = confirm("Wait! Don't leave yet! Can I chat with you?");
+//
+// if (userChoice) {
+//     console.log("yay!");
+// } else {
+//     console.log("boooo");
+// }
