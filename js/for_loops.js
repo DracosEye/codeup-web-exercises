1c8993e7c49b8a70ab5eea5a6d9956275e1d9010
Ben Heller <benheller1016@gmail.com>
2023-09-14 14:18:00 -0500

    Completed while loop

--- js/for_loops.js
+++ js/for_loops.js
@@ -27,4 +27,4 @@ function printFiveMults() {
     }
 }
 
-printFiveMults();
+// printFiveMults();
---
+++ js/while.js
@@ -0,0 +1,9 @@
+function powersOfTwo() {
+    let power = 1;
+    while (power <= 16) {
+        console.log(2 ** power);
+        power++;
+    }
+}
+
+powersOfTwo();
