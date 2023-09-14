9a0dff15b8e91dbda51cff5eb776ffdfd9e6ed30
Ben Heller <benheller1016@gmail.com>
2023-09-14 14:12:35 -0500

    Completed break and continue

---
+++ js/break_and_continue.js
@@ -0,0 +1,18 @@
+function printOdds() {
+    let skipNum = 0;
+    while (true) {
+        skipNum = prompt("Enter an odd number between 1 and 50")
+        if (skipNum >= 1 && skipNum <= 50 && skipNum % 2 == 1) {
+            break;
+        }
+    }
+    for (let i = 1; i < 50; i += 2) {
+        if (i == skipNum) {
+            console.log(`Yikes! Skipping number: ${skipNum}`);
+            continue;
+        }
+        console.log(`Here is an odd number: ${i}`);
+    }
+}
+
+printOdds();
