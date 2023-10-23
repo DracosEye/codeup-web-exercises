f71bae99898febb566dad669c5d8082767eb0f13
Ben Heller <benheller1016@gmail.com>
2023-10-23 13:30:58 -0500

    Completed request and response exercise

---
+++ js/weather_map.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// For current weather
+// fetch(`https://api.openweathermap.org/data/2.5/weather?` +
+//     `lat=29.426825118534886&lon=-98.48948239256946` +
+//     `&appid=${OPENWEATHER_API_KEY}&units=imperial` +
+//     `&units=imperial`)
+//     .then( data => data.json())
+//     .then( currentWeather => console.log(currentWeather));
+
+const weatherOutput = document.querySelector("#forecast");
+
+// For forecast weather
+fetch(`https://api.openweathermap.org/data/2.5/forecast?` +
+    `lat=29.426825118534886&lon=-98.48948239256946` +
+    `&appid=${OPENWEATHER_API_KEY}` +
+    `&units=imperial`)
+    .then( data => data.json())
+    .then( forecast => {
+        console.log(forecast);
+        forecast.list.forEach(weather => {
+            const time = document.createElement("p");
+            const temp = document.createElement("p");
+            time.innerText = dateFromTimeStamp(weather.dt);
+            temp.innerText = weather.main.temp;
+            weatherOutput.appendChild(time);
+            weatherOutput.appendChild(temp);
+        })
+    });
